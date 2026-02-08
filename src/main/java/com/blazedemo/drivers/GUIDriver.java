0aed75f44f8dfc4d8b96a553d96c52d5521fe7fc
Mohammed <Mohammedoghanem17@gmail.com>
2026-02-08 10:01:11 +0200

    Add Time Helpers

--- src/main/java/com/blazedemo/drivers/GUIDriver.java
+++ src/main/java/com/blazedemo/drivers/GUIDriver.java
@@ -1,11 +1,12 @@
 package com.blazedemo.drivers;
 
+import com.blazedemo.utils.dataReader.PropertyReader;
 import com.blazedemo.utils.logs.LogsManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ThreadGuard;
 
 public class GUIDriver {
-    private final String browser=PropertyReader.getProperty("browserType");
+    private final String browser=  PropertyReader.getProperty("browserType");
 
     private  ThreadLocal<WebDriver> driverThreadLocal = new ThreadLocal<>();
 
---
+++ src/main/java/com/blazedemo/utils/TimeManager.java
@@ -0,0 +1,17 @@
+package com.blazedemo.utils;
+
+import static java.lang.System.currentTimeMillis;
+
+public class TimeManager {
+    // screenshot - logs - reports
+  public static String getTimeStamp(){
+      return new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
+  }
+  // unique timestamp
+  public static String getSimpleTimestamp(){
+
+     return  Long.toString(currentTimeMillis());
+  }
+
+
+}
